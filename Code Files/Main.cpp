fbbc7889d61fba87a9f67ca1c4079cfcf22e56f7
wongksryan <wongksryan@gmail.com>
2026-02-11 11:27:51 -0500

    calibration

--- Code Files/Main.cpp
+++ Code Files/Main.cpp
@@ -1,1 +1,20 @@
 #include <cmath>
+#include "iq_cpp.h"
+
+
+using namespace vex;
+
+// Configure sensors
+void configureAllSensors()
+{
+Brain.Screen.print("Calibrating");
+BrainInertial.calibrate();
+while (BrainInertial.isCalibrating())
+{}
+Brain.Screen.clearScreen();
+Brain.Screen.setCursor(1,1);
+BrainInertial.resetRotation();
+driveMotors.resetPosition();
+Optical3.setLight(ledState::on);
+}
+
